2b6078a3ce830a581a9efcfbfa24c0a35ae145fb
Matheus Galdino <matheus.kho@gmail.com>
2022-05-20 19:14:15 -0300

    Preparando revisão de código

--- schemas/signInSchema.js
+++
@@ -1,8 +0,0 @@
-import Joi from "joi";
-
-const signInSchema = Joi.object({
-	email: Joi.string().email().required(),
-	password: Joi.string().min(1).required(),
-});
-
-export default signInSchema;
--- schemas/transactionSchema.js
+++
@@ -1,8 +0,0 @@
-import Joi from "joi";
-
-const transactionSchema = Joi.object({
-	value: Joi.string().min(1).required(),
-	description: Joi.string().min(1).required(),
-});
-
-export default transactionSchema;
